53a237eef5c95f83e9a12286aa0a76d5756a01b7
Dryzera <endryusdaniel6321@gmail.com>
2024-12-31 17:00:35 -0300

    create gerador.py, where will be generate the docx files. This is my last commit on year of 2024, my englesh is no well and i am progress in programming, i am much grateful for year, let it come a best. Happy New Year, I from future!

---
+++ home/planejamento/gerador.py
@@ -0,0 +1,49 @@
+from docx import Document
+from docx.shared import Inches
+from datetime import datetime
+
+# start code, this is an example
+def init_generate_document(matters: list): pass
+
+DEFAULT_SAVE_FOLDER = 'files_docx_generated'
+
+document = Document()
+
+document.add_heading('Document Title', 0)
+
+p = document.add_paragraph('A plain paragraph having some ')
+p.add_run('bold').bold = True
+p.add_run(' and some ')
+p.add_run('italic.').italic = True
+
+document.add_heading('Heading, level 1', level=1)
+document.add_paragraph('Intense quote', style='Intense Quote')
+
+document.add_paragraph(
+    'first item in unordered list', style='List Bullet'
+)
+document.add_paragraph(
+    'first item in ordered list', style='List Number'
+)
+
+
+records = (
+    (3, '101', 'Spam'),
+    (7, '422', 'Eggs'),
+    (4, '631', 'Spam, spam, eggs, and spam')
+)
+
+table = document.add_table(rows=1, cols=3)
+hdr_cells = table.rows[0].cells
+hdr_cells[0].text = 'Qty'
+hdr_cells[1].text = 'Id'
+hdr_cells[2].text = 'Desc'
+for qty, id, desc in records:
+    row_cells = table.add_row().cells
+    row_cells[0].text = str(qty)
+    row_cells[1].text = id
+    row_cells[2].text = desc
+
+document.add_page_break()
+
+document.save(DEFAULT_SAVE_FOLDER)
