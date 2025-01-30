7e8f39f545437b523f321140ba587e7ad8d5815e
Dryzera <endryusdaniel6321@gmail.com>
2025-01-30 12:03:00 -0300

    create model User Profile, when go stock user informations

--- home/admin.py
+++ home/admin.py
@@ -1,6 +1,12 @@
 from django.contrib import admin
-from home.models import Matter, School
+from home.models import Matter, School, UserProfile
 
-# Register your models here.
 admin.site.register(Matter)
 admin.site.register(School)
+
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    list_display = ('user', 'date_payment', 'date_expiration')
+    list_filter = ('date_payment', 'date_expiration')
+    search_fields = ('user__username', 'user__first_name', 'user__last_name')
+    ordering = ('date_expiration',)
