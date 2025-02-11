71612fe686ad45e21286853fff8d2b510ffbbbd7
Dryzera <endryusdaniel6321@gmail.com>
2025-02-11 18:54:19 -0300

    added a view for detail of activity, and created your template

--- home/views/activities_view.py
+++ home/views/activities_view.py
@@ -1,5 +1,7 @@
 from django.views.generic import ListView, DetailView
 from home.models import Activities
+from django.shortcuts import get_object_or_404
+
 
 class ActivityRead(ListView):
     template_name = 'activities/activities.html'
@@ -15,8 +17,11 @@ class ActivityView(DetailView):
     template_name = 'activities/detail.html'
     model = Activities
 
+    def get_object(self):
+        return get_object_or_404(Activities, slug=self.kwargs['slug'])
+
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['activities'] = Activities.objects.all()
-        context['site_title'] = 'Atividades - '
+        context['site_title'] = 'Atividade - '
+        context['activity'] = self.get_object()
         return context
