5cfbe75e7ddbfe2124aef5ed66e917763926ae1a
Dryzera <endryusdaniel6321@gmail.com>
2025-02-18 19:44:39 -0300

    making ajusts for responsivitie on create planning page

--- base_static/global/js/caracteresCount.js
+++ base_static/global/js/caracteresCount.js
@@ -1,10 +1,16 @@
 (() => {
+    const widthCols = window.innerWidth * 0.04;
     const fields = document.querySelectorAll('.form-area')
     let maxCaracteres = 700;
+
+    aditionalContent = document.getElementById('aditional_content')
+    aditionalContent.setAttribute('cols', String(widthCols))
     
     fields.forEach((value) => {
         const counter = value.querySelector('.count-caracteres')
         const input = value.querySelector('.textarea-create-planning')
+
+        input.setAttribute('cols', String(widthCols))
 
         input.addEventListener('input', () => {
             let caracteresInputed = input.value.length
