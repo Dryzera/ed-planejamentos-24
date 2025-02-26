c6038ed8c736e3c3e0c8f58d4f365dcd02fc5802
Dryzera <endryusdaniel6321@gmail.com>
2025-02-26 17:17:17 -0300

    change simbol + to / on convert_to_docx

--- home/utils/convert.py
+++ home/utils/convert.py
@@ -2,8 +2,8 @@ import subprocess
 from django.conf import settings
 
 def convert_to_docx(slug):
-    docx_path = settings.MEDIA_ROOT + f'files_docx_generated/planejamento_{slug}.docx'
-    pdf_path = settings.MEDIA_ROOT + f'files_docx_generated/planejamento_{slug}.pdf'
+    docx_path = settings.MEDIA_ROOT / f'files_docx_generated/planejamento_{slug}.docx'
+    pdf_path = settings.MEDIA_ROOT / f'files_docx_generated/planejamento_{slug}.pdf'
     try:
         subprocess.run(["pandoc", docx_path, "-o", pdf_path], check=True)
         print(f"PDF salvo em: {pdf_path}")
