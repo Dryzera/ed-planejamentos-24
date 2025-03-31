099e46416dc6992d14c42a0d41920316c036d7fd
Dryzera <endryusdaniel6321@gmail.com>
2025-03-31 13:34:42 -0300

    fixing bug on loader: he did not show ip

--- base_static/global/js/loader.js
+++ base_static/global/js/loader.js
@@ -1,5 +1,9 @@
-export default (function () {
-    window.addEventListener('load', function () {
-        document.getElementById('loader').style.display = 'none';
+export default (function() {
+    document.addEventListener("DOMContentLoaded", function () {
+        document.getElementById("loader").style.display = "flex";
+
+        window.addEventListener("load", function () {
+            document.getElementById("loader").style.display = "none";
+        });
     });
 })();
