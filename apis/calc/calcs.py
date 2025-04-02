ecb9ffa1da50584f01a7888764f66afc54f73c51
Dryzera <endryusdaniel6321@gmail.com>
2025-04-01 21:51:15 -0300

    created a new count for calcs

--- apis/calc/calcs.py
+++ apis/calc/calcs.py
@@ -1,19 +1,15 @@
 from abc import ABC, abstractmethod
 import math
-from os import name
 
-# Interface de Estratégia
 class OperacaoCalculadora(ABC):
     @abstractmethod
     def calcular(self, *args):
         pass
 
-# Estratégia de Logaritmo
 class Logaritmo(OperacaoCalculadora):
     def calcular(self, numero, base):
         return round(math.log(numero, base), 2)
 
-# Estratégia de Equação de Segundo Grau
 class EquacaoSegundoGrau(OperacaoCalculadora):
     def calcular(self, a, b, c):
         discriminante = b**2 - 4*a*c
@@ -22,8 +18,11 @@ class EquacaoSegundoGrau(OperacaoCalculadora):
         x1 = (-b + math.sqrt(discriminante)) / (2 * a)
         x2 = (-b - math.sqrt(discriminante)) / (2 * a)
         return (x1, x2)
+    
+class EquacaoPrimeiroGrau(OperacaoCalculadora):
+    def calcular(self, valor_x, valores):
+        return valores / valor_x
 
-# Contexto (Calculadora)
 class Calculadora:
     def __init__(self, operacao: OperacaoCalculadora):
         self.operacao = operacao
@@ -34,10 +33,12 @@ class Calculadora:
     def calcular(self, *args):
         return self.operacao.calcular(*args)
 
-if name == '__main__':
-    # Uso
+if __name__ == '__main__':
     calculadora = Calculadora(Logaritmo())
-    print(calculadora.calcular(100000, 3))  # Exemplo de logaritmo
+    print(calculadora.calcular(100000, 3))
 
     calculadora.set_operacao(EquacaoSegundoGrau())
-    print(calculadora.calcular(1, -3, 2))  # Exemplo de equação quadrática
+    print(calculadora.calcular(1, -3, 2))
+
+    calculadora.set_operacao(EquacaoPrimeiroGrau())
+    print(calculadora.calcular(8, 2))
--- apis/calc/dispatch.py
+++ apis/calc/dispatch.py
@@ -1,17 +1,19 @@
-from abc import abstractmethod, ABC
-from calcs import Calculadora, Logaritmo, EquacaoSegundoGrau
+from calcs import *
 
 class CalculadoraFactory:
     @staticmethod
     def get_operacao(tipo):
-        if tipo == "logaritmo":
-            return Logaritmo()
-        elif tipo == "equacao_segundo_grau":
-            return EquacaoSegundoGrau()
-        else:
-            raise ValueError("Operação desconhecida")
+        match tipo:
+            case 'logaritmo':
+                return Logaritmo()
+            case "equacao_segundo_grau":
+                return EquacaoSegundoGrau()
+            case 'equacao_primeiro_grau':
+                return EquacaoPrimeiroGrau()
+            case _:
+                raise ValueError("Operação desconhecida")
 
 # Uso
-operacao = CalculadoraFactory.get_operacao("logaritmo")
+operacao = CalculadoraFactory.get_operacao("equacao_primeiro_grau")
 calculadora = Calculadora(operacao)
-print(calculadora.calcular('x-2', 2))
+print(calculadora.calcular(-2,-6))
